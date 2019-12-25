3ed5fb559ada48b0b5cdea229fa25ecd6c5cf678
takahashi-shinhi-002 <switchscratch015@gmail.com>
2019-12-25 19:17:46 +0900

    PinBall 1回目のコミット
    課題両方提出

---
+++ Assets/FripperController.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class FripperController : MonoBehaviour
+{
+    //HingeJointコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+
+    //初期の傾き
+    private float defaultAngle = 20;
+    //弾いた時の傾き
+    private float flickAngle = -20;
+
+    // 右画面の指の識別
+    public int RightFingerid;
+
+    // 左画面の指の識別
+    public int LeftFingerid;
+
+    // Use this for initialization
+    void Start()
+    {
+        //HingeJointコンポーネント取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        //フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // 左矢印キーを押した時左フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+        // 右矢印キーを押した時右フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        // 矢印キー離された時フリッパーを元に戻す
+        if (Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+
+        if (Input.touchCount > 0)
+        {
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                //タッチした指の情報
+                Touch touch = Input.GetTouch(i);
+
+
+                // 座標xがスクリーンの2分の1以上の場合
+                if (touch.position.x >= Screen.width / 2)
+                {
+                    // 右側をタップしたら右のフリッパーが動く
+                    if (touch.phase == TouchPhase.Began && tag == "RightFripperTag")
+                    {
+                        SetAngle(this.flickAngle);
+                        RightFingerid = touch.fingerId;
+                    }
+                }
+                // 左側をタップしたら左のフリッパーが動く
+                else if (touch.phase == TouchPhase.Began && tag == "LeftFripperTag")
+                {
+                    SetAngle(this.flickAngle);
+                    LeftFingerid = touch.fingerId;
+                }
+
+
+                // タップが終了したらフリッパーが元に戻る
+                if (touch.phase == TouchPhase.Ended && tag == "LeftFripperTag")
+                {
+                    if(touch.fingerId == LeftFingerid)
+                    {
+                        SetAngle(this.defaultAngle);
+                    }
+                }
+
+                if (touch.phase == TouchPhase.Ended && tag == "RightFripperTag")
+                {
+                    if (touch.fingerId == RightFingerid)
+                    {
+                        SetAngle(this.defaultAngle);
+                    }
+                }
+            }
+        }
+    }
+
+    //フリッパーの傾きを設定
+    public void SetAngle(float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+}
---
+++ Assets/ScoreController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour {
+
+    private GameObject scoreText;
+
+    private int score;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.scoreText = GameObject.Find("ScoreText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         this.scoreText.GetComponent<Text>().text = "score: " + score;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            score += 10;
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            score += 20;
+        }
+        else if (other.gameObject.tag == "SmallCloudTag")
+        {
+            score += 30;
+        }
+        else if (other.gameObject.tag == "LargeCloudTag")
+        {
+            score += 50;
+        }
+    }
+}
+    
+
+
+
+
